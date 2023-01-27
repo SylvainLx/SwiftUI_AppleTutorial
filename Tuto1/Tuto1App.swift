52a5677ce29c4c49ba0c1f69b58057b2cf84aea9
Sylvain LEGUAY <leguay.sylvain40@gmail.com>
2023-01-27 14:29:10 +0100

    Initial Commit

---
+++ Tuto1/Tuto1App.swift
@@ -0,0 +1,17 @@
+//
+//  Tuto1App.swift
+//  Tuto1
+//
+//  Created by Sylvain Leguay on 27/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Tuto1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
